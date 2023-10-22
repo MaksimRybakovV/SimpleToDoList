9e4ad7c60eb7abe0d14ee3f8d4b5edfcf0499134
MaksimRybakovV <mrybakov1406@gmail.com>
2023-10-22 19:29:07 +0500

    Added authentication/authorization.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
 using WebApi.Data;
 using WebApi.Services.AuthotizationService;
 using WebApi.Services.TodoTaskService;
@@ -21,7 +25,30 @@ namespace WebApi
             builder.Services.AddScoped<IAuthorizationService, AuthorizationService>();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                options.OperationFilter<SecurityRequirementsOperationFilter>();
+            });
+
+            builder.Services.AddAuthentication().AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                        .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value!)),
+                };
+            });
 
             var app = builder.Build();
 
